479966eef72a3c229ae85e8bb73ec2f00f73cf67
mustafa oğuzhan ün <mustafaoguzhanun@mustafa-MacBook-Air.local>
2019-02-05 00:23:02 +0300

    correction2

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp",{ useNewUrlParser: true }).then(()=>{
+mongoose.connect("mongodb://elsamsuni:elsamsuni55@ds221645.mlab.com:21645/todoapp" || "mongodb://localhost:27017/TodoApp",{ useNewUrlParser: true }).then(()=>{
     console.log("Connected to Database...")   
 });
 
